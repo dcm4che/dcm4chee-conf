598b5b034b60ade980c914360b179c6b6d271c80
Greg Knox <greg.knox@agfa.com>
2015-06-02 14:11:21 -0400

    [ARCH-274] Initialize the wrapper once instead of per call

--- dcm4chee-conf-decorators/src/main/java/org/dcm4chee/conf/decorators/DynamicDecoratorWrapper.java
+++ dcm4chee-conf-decorators/src/main/java/org/dcm4chee/conf/decorators/DynamicDecoratorWrapper.java
@@ -11,23 +11,37 @@ public class DynamicDecoratorWrapper<T> {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(DynamicDecoratorWrapper.class);
 
-	 @Inject
-	 @ConfiguredDynamicDecorators
-	 Instance<Collection<DelegatingServiceImpl<T>>> dynamicDecorators;
+	@Inject
+	@ConfiguredDynamicDecorators
+	Instance<Collection<DelegatingServiceImpl<T>>> dynamicDecorators;
+	
+	private T wrappedDecorator = null;
 	
     public final T wrapWithDynamicDecorators(T delegate) {
-    	DelegatingServiceImpl<T> theService = new DelegatingServiceImpl<T>();
-    	theService.setOrig(delegate);
-
-    	for (Collection<DelegatingServiceImpl<T>> collectionDynamicDecoratorsForType : dynamicDecorators) {
-	        for (DelegatingServiceImpl<T> dynamicDecorator : collectionDynamicDecoratorsForType) {
-	            LOG.trace("Iterating over {}", dynamicDecorator.getClass());
-	        	dynamicDecorator.setDelegate(theService);
-	            dynamicDecorator.setOrig(dynamicDecorator.getTypeObject());
-	            theService = dynamicDecorator;
-	        }
+    	if (wrappedDecorator == null) {
+    		getWrappedDynamicDecorators(delegate);
     	}
-
-        return theService.getOrig();
+    	LOG.trace("Returning {}.", wrappedDecorator);
+    	
+    	return wrappedDecorator;
     }
+    
+    private synchronized void getWrappedDynamicDecorators(T delegate) { 
+    	if (wrappedDecorator != null) {
+    		return;
+    	}
+    	DelegatingServiceImpl<T> theService = new DelegatingServiceImpl<T>();
+	    theService.setOrig(delegate);
+	    
+	    for (Collection<DelegatingServiceImpl<T>> collectionDynamicDecoratorsForType : dynamicDecorators) {
+	    	for (DelegatingServiceImpl<T> dynamicDecorator : collectionDynamicDecoratorsForType) {
+	    		LOG.trace("Iterating over {}", dynamicDecorator.getClass());
+	    		dynamicDecorator.setDelegate(theService);
+	    		dynamicDecorator.setOrig(dynamicDecorator.getTypeObject());
+	    		theService = dynamicDecorator;
+	    	}
+	    }
+	    
+	    wrappedDecorator = theService.getOrig();
+	}
 }
