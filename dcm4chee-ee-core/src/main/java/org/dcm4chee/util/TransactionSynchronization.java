6c037db326d9da97fccce90572e4c0f3a005e4ef
alexander.hoermandinger <alexander.hoermandinger@agfa.com>
2015-12-02 13:06:20 +0100

    fix: Possible bug with executing on-commit logic for nested JTA transactions
    The TransactionSynchronizer implementation did not take into account that JTA transactions might be nested (using REQUIRES_NEW for example). This could lead to the wrong behavior that all on-commit tasks are executed for the wrong transaction

--- dcm4chee-ee-core/src/main/java/org/dcm4chee/util/TransactionSynchronization.java
+++ dcm4chee-ee-core/src/main/java/org/dcm4chee/util/TransactionSynchronization.java
@@ -6,15 +6,17 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.*;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 @ApplicationScoped
 public class TransactionSynchronization {
 
     private static final Logger LOG = LoggerFactory.getLogger(TransactionSynchronization.class);
-
-    private static final ThreadLocal<List<Runnable>> toRunOnCommit = new ThreadLocal<>();
+    
+    private static final String ON_COMMIT_RUNNER_TX_RESOURCE = OnCommitRunner.class.getName();
 
     @Resource(lookup = "java:jboss/TransactionManager")
     private TransactionManager tmManager;
@@ -42,54 +44,104 @@ public class TransactionSynchronization {
     }
 
     /**
-     * Executes r once (and only if) the current transaction was successfully committed.
+     * Executes a given runnable once (and only if) the current transaction was successfully committed.
      * For multiple calls, the order of execution is preserved.
-     * If called without a transaction - simply executes r right away
-     *
+     * </p>
+     * <ul>
+     * <li>If called within a transaction then the runnable is called after the ongoing transaction has been 
+     * successfully committed (=AFTER_COMPLETION phase). The runnable is executed in the thread that
+     * committed the transaction</li>
+     * <li>If called without a transaction then the runnable is simply execute right-away using the calling thread
+     *</ul>
      * @param r what to execute
      */
-    public void afterSuccessfulCommit(final Runnable r) {
+    public void afterSuccessfulCommit(Runnable r) {
+        afterSuccessfulCommit(r, CallingThreadExecutor.INSTANCE);
+    }
+    
+    /*
+     * Singleton executor that simply uses the calling thread for task execution
+     */
+    private enum CallingThreadExecutor implements Executor {
+        INSTANCE;
+        
+        @Override
+        public void execute(Runnable r) {
+            r.run();
+        }
+        
+    }
+    
+    /**
+     * Executes a given runnable once (and only if) the current transaction was successfully committed.
+     * For multiple calls, the order of execution is preserved.
+     * </p>
+     * <ul>
+     * <li>If called within a transaction then the runnable is called after the ongoing transaction has been 
+     * successfully committed (=AFTER_COMPLETION phase)</li>
+     * <li>If called without a transaction then the runnable is simply execute right-away using the calling thread
+     *</ul>
+     *In all cases the runnable is executed in the give executor
+     * @param r what to execute
+     * @param executor Executor used to execute the runnable
+     */
+    public void afterSuccessfulCommit(Runnable r, Executor executor) {
 
         try {
             Transaction transaction = tmManager.getTransaction();
 
             if (transaction == null) {
                 // if no tx - just execute the callback
-                r.run();
+                executor.execute(r);
             } else {
                 // otherwise register the callback if necessary and add to the list
-                if (toRunOnCommit.get() == null) {
-                    toRunOnCommit.set(new ArrayList<Runnable>());
-                    transaction.registerSynchronization(new OnCommitRunner());
+                OnCommitRunner onCommitRunner = (OnCommitRunner)synchronizationRegistry.getResource(ON_COMMIT_RUNNER_TX_RESOURCE);
+                if (onCommitRunner == null) {
+                    onCommitRunner = new OnCommitRunner(executor);
+                    transaction.registerSynchronization(onCommitRunner);
+                    synchronizationRegistry.putResource(ON_COMMIT_RUNNER_TX_RESOURCE, onCommitRunner);
                 }
-                toRunOnCommit.get().add(r);
+                
+                onCommitRunner.addRunAfterCommit(r);
             }
         } catch (SystemException | RollbackException e) {
             throw new RuntimeException("Cannot register on-commit hook", e);
         }
     }
-
+    
     private static class OnCommitRunner implements Synchronization {
+        private final Executor executor;
+        private final List<Runnable> toRunAfterCommit = new ArrayList<>();
+        
+        private OnCommitRunner(Executor executor) {
+            this.executor = executor;
+        }
+        
+        private void addRunAfterCommit(Runnable runnable) {
+            toRunAfterCommit.add(runnable);
+        }
+        
         @Override
         public void beforeCompletion() {
-
+            // NOP
         }
 
         @Override
         public void afterCompletion(int status) {
+            // run only if successfully committed
             try {
-                // run only if successfully committed
                 if (status == Status.STATUS_COMMITTED) {
-                    for (Runnable runnable : toRunOnCommit.get()) {
+                    for (Runnable runnable : toRunAfterCommit) {
                         try {
-                            runnable.run();
+                            executor.execute(runnable);
                         } catch (Exception e) {
-                            LOG.error("Error while executing a callback after transaction commit", e);
+                            LOG.error("Error while executing a callback after transaction commit",
+                                    e);
                         }
                     }
                 }
             } finally {
-                toRunOnCommit.remove();
+                toRunAfterCommit.clear();
             }
         }
     }
